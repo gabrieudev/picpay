bf469803598baeeaab0714d2aba7cd68a09f471d
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-01-04 18:38:06 -0300

    feat: criar modelos de persistência

---
+++ src/main/java/br/com/gabrieudev/picpay/infrastructure/persistence/models/WalletModel.java
@@ -0,0 +1,69 @@
+package br.com.gabrieudev.picpay.infrastructure.persistence.models;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+
+import br.com.gabrieudev.picpay.domain.entities.Wallet;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@EqualsAndHashCode(of = "id")
+@Table(name = "Wallets")
+public class WalletModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "fullName", nullable = false)
+    private String fullName;
+    
+    @Column(name = "cpfCnpj", nullable = false, unique = true)
+    private String cpfCnpj;
+    
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    
+    @Column(name = "password", nullable = false)
+    private String password;
+    
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+    
+    @ManyToOne
+    @JoinColumn(name = "walletTypeId")
+    private WalletTypeModel type;
+
+    public static WalletModel fromDomainObj(Wallet wallet) {
+        return new ModelMapper().map(wallet, WalletModel.class);
+    }
+
+    public Wallet toDomainObj() {
+        return new ModelMapper().map(this, Wallet.class);
+    }
+
+    public void deposit(BigDecimal value) {
+        this.balance = this.balance.add(value);
+    }
+
+    public void withdraw(BigDecimal value) {
+        this.balance = this.balance.subtract(value);
+    }
+}
---
+++ src/main/java/br/com/gabrieudev/picpay/infrastructure/persistence/models/WalletTypeModel.java
@@ -0,0 +1,45 @@
+package br.com.gabrieudev.picpay.infrastructure.persistence.models;
+
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+
+import br.com.gabrieudev.picpay.domain.entities.WalletType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@EqualsAndHashCode(of = "id")
+@Table(name = "WalletTypes")
+public class WalletTypeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public static WalletTypeModel fromDomainObj(WalletType walletType) {
+        return new ModelMapper().map(walletType, WalletTypeModel.class);
+    }
+
+    public WalletType toDomainObj() {
+        return new ModelMapper().map(this, WalletType.class);
+    }
+}
